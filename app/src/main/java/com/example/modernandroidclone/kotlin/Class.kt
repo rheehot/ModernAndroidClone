1285867db4f96ec8be3cabb2ff1d1d14f14207f7
sery270 <sery270@naver.com>
2021-01-29 23:40:07 +0900

    📝 클래스, init

---
+++ app/src/main/java/com/example/modernandroidclone/kotlin/Class.kt
@@ -0,0 +1,34 @@
+package com.example.modernandroidclone.kotlin
+
+// 클래스
+
+fun classFunc() {
+    println("**from classFunc**")
+
+    val person = Person("박세란")
+
+    // 굳이 getter, setter 정의 없이 멤버 접근 가능
+    person.name = "한석봉"
+    println("person 멤버 변수 name의 상태 : ${person.name}")
+
+    println("Animal 생성 전")
+    val animal = Animal()
+    println("**from classFunc**")
+    println("Animal 생성 후")
+    println("Animal의 멤버 변수 name의 상태 : ${animal.name}")
+
+}
+
+// class는 기본이 public 임
+class Person(var name: String)
+
+class Animal() {
+
+    val name = "rabbit"
+
+    // init은 생성자가 호출되면 실행되는 블럭
+    init {
+        println("**from init block at class Animal**")
+        println("Animal의 생성자가 호출되어 init 블럭이 실행 됨 !")
+    }
+}
--- app/src/main/java/com/example/modernandroidclone/kotlin/Main.kt
+++ app/src/main/java/com/example/modernandroidclone/kotlin/Main.kt
@@ -3,6 +3,7 @@ package com.example.modernandroidclone.kotlin
 // 코틀린 환경 구성
 
 fun main() {
-   data()
-   logic()
+//   data()
+//   logic()
+   classFunc()
 }
